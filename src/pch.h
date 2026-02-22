d06f3f270e23e6893b385f3fec3bf5f497a309bb
deadsoftie <rahulnair1229@gmail.com>
2026-02-22 15:28:08 -0800

    Migrated to vcpkg for dependency management

--- src/pch.h
+++ src/pch.h
@@ -27,5 +27,5 @@
 // ImGui
 // -----------------------------
 #include <imgui.h>
-#include <backends/imgui_impl_glfw.h>
-#include <backends/imgui_impl_opengl3.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
