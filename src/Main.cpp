4f8a5cb8e15b418250a18cf50d748d1b7be6c398
deadsoftie <rahulnair1229@gmail.com>
2026-09-10 14:38:58 -0700

    Added GPU preference

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,15 @@
 
 #include "App.h"
 
+#ifdef TRL_PLATFORM_WINDOWS
+extern "C"
+{
+    // GPU preferred, falls back to integrated if not present
+    __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif
+
 int main()
 {
     App app;
